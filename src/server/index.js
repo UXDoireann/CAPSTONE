efcc6daf05a6cd806d35ca8a571bf9e6e8ad4490
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-03-04 15:37:31 +0000

    Set-up express server

---
+++ src/server/index.js
@@ -0,0 +1,29 @@
+projectData={};
+
+//Express to run server
+const express = require('express');
+
+//Set-up instance of app
+const app = express();
+
+//Middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+//Initialize main project folder
+app.use(express.static('dist'));
+
+// Setup Server
+const port = 5000;
+
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost:${port}`);
+};
