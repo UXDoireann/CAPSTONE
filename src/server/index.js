bb464d5df82e9570115de3c42aaffc52a17831e4
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-03-05 14:20:22 +0000

    Install Webpack Dev Server

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 
 //Middleware
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
