cad55118a401401c40fbbd638a1e64def3f8db9b
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-03-22 18:12:10 +0000

    Feat: Add Readme info

---
+++ Readme.md
@@ -0,0 +1,33 @@
+/**UDACITY FRONT-END DEVELOPMENT CAPSTONE PROJECT**
+
+//**Travel App **
+This travel app allows users to enter their destination city and departure date.
+It then displays the weather forecast for their arrival date, information about the destination country and an image.
+
+///**Key Project Tasks**
+- Use GeoNames.org API to fetch location data of entered city
+
+- Use Weatherbit.io API to get weather for arrival date with icons
+
+- Use Pixabay.com API to display image of destination city 
+
+- Use webpack for development and production builds
+
+- Use jest to test
+
+- Install service workers
+
+///**Instructions**
+- git clone this repo
+
+- Install npm
+
+- Get API keys for Weatherbit and Pixabay and a username for GeoNames.org. 
+  Write them in an .env file, giving them matching names to those in the server/index.js file
+
+ - In the terminal, run: npm run build-prod
+
+- Go to http://localhost:5050/
+
+////Illustration
+UI illustration made by UXDoireann 
--- src/client/js/picture.js
+++ src/client/js/picture.js
@@ -17,15 +17,18 @@ fetch('http://localhost:5050/addPic', {
 })
 .then(res => res.json())
 .then(function(data) {
+   
+
     if(data.totalHits == 0){
+        console.log("default pic");
+        document.querySelector('#city_pic').src = "https://pixabay.com/get/gbfdc0e29c4348801cf0dab0ae71484bdcc6fff35750c9551307a971b80cc0d66002ae840dc025cfce063cb8b63b648ac614d784f0c78827505e881388b6d2ae5_640.jpg";
         document.querySelector('#city_pic').style.borderRadius = "15px";
         document.querySelector('#city_pic').style.boxShadow= "10px 20px 30px lightblue";
-        document.querySelector('#city_pic').src = "https://pixabay.com/get/g6453f22984f68e30c20149faf738d8252904018c04cf620263cb206076c77a3bd91f4d362e0ec5edd0403ab87ba9a349_640.jpg";
-        
+       
         
     }else{
         document.querySelector('#city_pic').style.borderRadius = "15px";
-        document.querySelector('#city_pic').style.boxShadow= "10px 20px 30px lightblue";
+       document.querySelector('#city_pic').style.boxShadow= "10px 20px 30px lightblue";
         document.querySelector('#city_pic').src=data.hits[0].webformatURL;
         
         console.log(data.hits[0].webformatURL);
